a735a8416f682f09d10b5542aa3118f270246ab8
Mohammad Monir <iammohammadmonir@gmail.com>
2023-02-28 15:07:23 +0600

    Fix error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import About from './components/About';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Hero from './components/Hero';
 import Product from './components/Product';
 import Projects from './components/Projects';
 import Services from './components/Services';
