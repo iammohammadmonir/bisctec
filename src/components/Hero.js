6ae7f88cc630f643064a66e42ab917b6d8692ea0
Mohammad Monir <iammohammadmonir@gmail.com>
2023-03-01 13:26:33 +0600

    update the video

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className='overlay'></div>
       <video src={videoBg} autoPlay loop muted />
       <div className='content'>
-        <h1>Welcome</h1>
+        <h1>big screen technology GmbH</h1>
       </div>
     </Wrapper>
   );
